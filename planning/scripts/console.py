7e2bc3bccff5530aceb68bc911e56070e4ca2f09
RTU <fumincha768@gmail.com>
2022-06-08 16:02:15 +0800

    Fixing GUI rendering

--- planning/scripts/console.py
+++ planning/scripts/console.py
@@ -19,37 +19,124 @@ from fleet_msgs.msg import PlanRequest, Path, Point, Status, FleetStatus
 from functools import partial
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+class GridMap(QtGui.QPolygon):
+    def __init__(self, rect, mapsize, node_radius, qp):
+        super().__init__()
+        self.rect = np.asarray(rect, dtype=int)
+        self.mapsize = np.asarray(mapsize, dtype=int)
+        self.node_radius = node_radius
+        self.grid_size = self.rect[2:]/(self.mapsize+1) #grid size
+        self.qp = qp
+
+        self.__rgb_vec = np.asarray(((np.cos(0.0), np.sin(0.0)), (np.cos(2.094395), np.sin(2.094395)),(np.cos(4.18879), np.sin(4.18879))), dtype=float)
+
+        w,h = self.mapsize
+        ox,oy,gw,gh = self.rect
+
+        self.x_mapping = np.linspace(ox, ox+gw, num=w+2, dtype=int)[1:-1]
+        self.y_mapping = np.linspace(oy, oy+gh, num=h+2, dtype=int)[1:-1]
+        
+        for x in self.x_mapping:
+            for y in self.y_mapping:
+                self << QtCore.QPoint(int(x), int(y))
+
+    def __rainbow(self, num):
+        angle = np.linspace(0.0, 3.14159*2.0*((num-1)/num), num=num)[:,np.newaxis]
+        vec = np.concatenate((np.cos(angle), np.sin(angle)), axis=1)
+        rgb = np.einsum('ij,kj -> ik', vec,self.__rgb_vec)
+        exp = np.exp(rgb)
+        rgb = exp/np.sum(exp,axis=1, keepdims=True)*255.0
+        return np.clip(rgb.astype(int), 0, 255)
+
+    def draw(self):
+
+        self.qp.setBrush(QtGui.QColor(0,0,0))
+        for i in range(self.count()):
+            self.qp.drawEllipse(self.at(i), self.node_radius, self.node_radius)
+
+    def getFocus(self, e: QtGui.QMouseEvent):
+        mouse = np.asarray((e.pos().x(), e.pos().y()), dtype=float)
+
+        mouse = (mouse - self.rect[:2] - self.grid_size)    #mouse position in subcoordinate
+        closest_target = np.round(mouse/self.grid_size).astype(int)
+        relative_position = closest_target*self.grid_size - mouse
+        if np.linalg.norm(relative_position) < self.node_radius:
+            return closest_target
+        return None
+
+    def drawNode(self, x, y, color):
+
+        self.qp.setBrush(QtGui.QColor(color[0],color[1],color[2]))
+        self.qp.drawEllipse(QtCore.QPoint(x,y), self.node_radius, self.node_radius)
+
+    def drawPath(self, path: Path, beg_color=(255,0,0), end_color=(0,0,255)):
+
+        shift = self.grid_size + self.rect[:2]
+
+        i,l = -1,len(path.path)
+        beg_color = np.array(beg_color)
+        end_color = np.array(end_color)
+
+        for p in path.path:
+            i+=1
+            fuse_color = (beg_color*(1.0-i/l) + end_color*i/l).astype(int)
+
+            pos = np.asarray((p.x,p.y), dtype=int)*self.grid_size + shift
+
+            self.drawNode(pos[0], pos[1], fuse_color)
+            
+    def drawMultiplePaths(self, paths, allstatus):
+
+        shift = self.grid_size + self.rect[:2]
+
+        paths = paths.values()
+        rainbow = self.__rainbow(len(paths))
+        for color,path in zip(rainbow,paths):
+            status = allstatus[path.target]
+            for p in path.path[status.current.id: status.current.id+3]:
+
+                pos = np.asarray((p.x,p.y), dtype=int)*self.grid_size + shift
+
+                self.drawNode(pos[0], pos[1], color)
+
 class PanelUI(QtWidgets.QMainWindow):
     '''
     The panel UI implemented with PyQT5
     '''
     def __init__(self, sendWaypointF, resetWaypointF, gridClickedF, sizeChangedF, changeTargetF, sendPlaneF):
         super().__init__()
-        self.Dialog = self
         self.resize(800, 600)
 
-        self.gridMap = QtWidgets.QFrame(self.Dialog)
-        self.gridMap.setObjectName("Grid Frame")
-        self.gridMap.setGeometry(QtCore.QRect(50, 50, 700, 400))
-        self.gridButtonList = []
+        # self.gridMap = QtWidgets.QFrame(self)
+        # self.gridMap.setObjectName("Grid Frame")
+        # self.gridMap.setGeometry(QtCore.QRect(50, 50, 700, 400))
+        # self.gridButtonList = []
+        self.qp = QtGui.QPainter(self)
+        self.draw_multi_targets = None
+        self.draw_target = None
 
-        self.planeButton = QtWidgets.QPushButton(self.Dialog)
+        self.planeButton = QtWidgets.QPushButton(self)
         self.planeButton.setGeometry(QtCore.QRect(520, 490, 80, 25))
         self.planeButton.setObjectName("planeButton")
         self.planeButton.setText('Planning')
 
-        self.resetButton = QtWidgets.QPushButton(self.Dialog)
+        self.resetButton = QtWidgets.QPushButton(self)
         self.resetButton.setGeometry(QtCore.QRect(620, 490, 80, 25))
         self.resetButton.setObjectName("resetPathButton")
         self.resetButton.setText('Reset')
 
-        self.sendButton = QtWidgets.QPushButton(self.Dialog)
+        self.sendButton = QtWidgets.QPushButton(self)
         self.sendButton.setGeometry(QtCore.QRect(520, 525, 80, 25))
         self.sendButton.setObjectName("sendPlan")
         self.sendButton.setText('Send Plan')
 
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.Dialog)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(70, 450, 200, 35))
+        self.sendButton = QtWidgets.QPushButton(self)
+        self.sendButton.setGeometry(QtCore.QRect(400, 525, 80, 25))
+        self.sendButton.setObjectName("setSize")
+        self.sendButton.setText('Set Map')
+
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(400, 450, 200, 35))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.MapSize = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.MapSize.setContentsMargins(0, 0, 0, 0)
@@ -61,15 +148,15 @@ class PanelUI(QtWidgets.QMainWindow):
         self.height.setObjectName("height")
         self.MapSize.addWidget(self.height)
 
-        self.listWidget = QtWidgets.QListWidget(self.Dialog)
-        self.listWidget.setGeometry(QtCore.QRect(70, 500, 200, 150))
-        self.listWidget.setObjectName("listWidget")
-        self.listWidget.addItem("dashboard")
-        self.listWidget.addItem("car_1")
-        self.listWidget.addItem("car_2")
-        self.listWidget.setCurrentRow(0)
+        self.targetlist = QtWidgets.QListWidget(self)
+        self.targetlist.setGeometry(QtCore.QRect(70, 450, 200, 100))
+        self.targetlist.setObjectName("targetlist")
+        self.targetlist.addItem("dashboard")
+        self.targetlist.addItem("car_1")
+        self.targetlist.addItem("car_2")
+        self.targetlist.setCurrentRow(0)
 
-        self.__retranslateUi(self.Dialog)
+        self.__retranslateUi(self)
 
         self.width.setText('10')
         self.height.setText('10')
@@ -86,107 +173,81 @@ class PanelUI(QtWidgets.QMainWindow):
         self.planeButton.clicked.connect(self.__sendWaypoints)
         self.resetButton.clicked.connect(self.__resetWaypoints)
         self.sendButton.clicked.connect(self.__sendPlane)
-        self.listWidget.itemClicked.connect(self.__changeTarget)
+        self.targetlist.itemClicked.connect(self.__changeTarget)
         self.width.textChanged.connect(self.__changeSize)
         self.height.textChanged.connect(self.__changeSize)
 
-        QtCore.QMetaObject.connectSlotsByName(self.Dialog)
+        QtCore.QMetaObject.connectSlotsByName(self)
 
         self.__changeSize()
-        self.Dialog.show()
+        self.show()
 
-        self.__rgb_vec = np.asarray(((np.cos(0.0), np.sin(0.0)), (np.cos(2.094395), np.sin(2.094395)),(np.cos(4.18879), np.sin(4.18879))), dtype=float)
-
-        self.points = QtGui.QPolygon()
-
+    
     def mouseMoveEvent(self, e: QtGui.QMouseEvent):
-        self.points << e.pos()
-        self.update()
+        target=self.gridMap.getFocus(e)
+        if target is not None:
+            print(target)
+            self.update()
 
     def mousePressEvent(self, e):
-        self.points << e.pos()
-        self.update()
+        target=self.gridMap.getFocus(e)
+        if target is not None:
+            self.gridClickedF(target[0], target[1], self.getTarget())
+            self.update()
         
     def paintEvent(self, ev):
-        qp = QtGui.QPainter(self)
-        qp.setRenderHint(QtGui.QPainter.Antialiasing)
-        pen = QtGui.QPen(QtCore.Qt.red, 5)
-        brush = QtGui.QBrush(QtCore.Qt.red, QtCore.Qt.BrushStyle.Dense2Pattern)
-        #qp.setPen(pen)
-        qp.setBrush(brush)
-        for i in range(self.points.count()):
-            qp.end
-            qp.drawPoints(self.points)
-
+        if not self.qp.isActive():
+            self.qp.begin(self)
+        self.gridMap.draw()
+        if self.draw_target is not None:
+            self.gridMap.drawPath(self.draw_target)
+        if self.draw_multi_targets is not None:
+            self.gridMap.drawMultiplePaths(self.draw_multi_targets[0], self.draw_multi_targets[1])
+        self.qp.end()
     '''
     Private functions
     '''
-    def __rgb2hex(self, r, g, b):
-        return '#%02x%02x%02x'%(int(r), int(g), int(b))
-
-    def __rainbow(self, num):
-        angle = np.linspace(0.0, 3.14159*2.0*((num-1)/num), num=num)[:,np.newaxis]
-        vec = np.concatenate((np.cos(angle), np.sin(angle)), axis=1)
-        rgb = np.einsum('ij,kj -> ik', vec,self.__rgb_vec)
-        exp = np.exp(rgb)
-        rgb = exp/np.sum(exp,axis=1, keepdims=True)*255.0
-        return np.clip(rgb.astype(int), 0, 255)
-
-    def __setButtonColor(self, rgb=(0,0,0), x=-1, y=-1, button : QtWidgets.QPushButton = None) -> QtWidgets.QPushButton:
-        if button is None:
-            button = self.gridButtonList[x][y]
-        #button.setStyleSheet(f"background-color: {self.__rgb2hex(rgb[0], rgb[1], rgb[2]) if hex is None else hex}")
-
-        button.setAutoFillBackground(True)
-        palette = button.palette()
-        palette.setColor(button.backgroundRole(), QtGui.QColor(rgb[0], rgb[1], rgb[2]))
-        button.setPalette(palette)
-
-        return button
 
     def __resetGridMap(self):
         for button in self.__iterButton():
             self.__setButtonColor(rgb=(255,255,255),button=button).setText('')
                
-    def __iterButton(self):
-        for row in self.gridButtonList:
-            for b in row:
-                yield b
-
     def __retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
 
     # callbacks of weidgets
     def __changeSize(self):
-        w,h = self.getMapSize()
-        gw,gh = self.gridMap.geometry().width(),self.gridMap.geometry().height()
-        sx, sy = gw/w, gh/h
 
-        buttonSize = min(30,sx,sy)
-        offset = (sx/2-buttonSize/2, sy/2-buttonSize/2)
+        self.gridMap = GridMap((50, 50, 700, 400), (self.getMapSize()), 15, qp=self.qp)
+        # w,h = self.getMapSize()
+        # gw,gh = self.gridMap.geometry().width(),self.gridMap.geometry().height()
+        # sx, sy = gw/w, gh/h
 
-        if w<=0 or h<=0:
-            return
+        # buttonSize = min(30,sx,sy)
+        # offset = (sx/2-buttonSize/2, sy/2-buttonSize/2)
 
-        for b in self.__iterButton():
-            b.deleteLater()
-            b.setVisible(False)
+        # if w<=0 or h<=0:
+        #     return
+
+        # for b in self.__iterButton():
+        #     b.deleteLater()
+        #     b.setVisible(False)
         
-        col=[]
-        for x in range(w):
-            row = []
-            for y in range(h):
-                button = QtWidgets.QPushButton(self.gridMap)
-                button.setGeometry(QtCore.QRect(x*sx+offset[0],y*sy+offset[1], buttonSize, buttonSize))
-                button.setObjectName(f"{x}_{y}")
-                self.__setButtonColor(rgb=(255,255,255),button=button)
-                button.clicked.connect(partial(self.__gridClicked, button, x, y))
-                row.append(button)
-            col.append(row)
-        self.gridButtonList=col
+        # col=[]
+        # for x in range(w):
+        #     row = []
+        #     for y in range(h):
+        #         button = QtWidgets.QPushButton(self.gridMap)
+        #         button.setGeometry(QtCore.QRect(x*sx+offset[0],y*sy+offset[1], buttonSize, buttonSize))
+        #         button.setObjectName(f"{x}_{y}")
+        #         self.__setButtonColor(rgb=(255,255,255),button=button)
+        #         button.clicked.connect(partial(self.__gridClicked, button, x, y))
+        #         row.append(button)
+        #     col.append(row)
+        # self.gridButtonList=col
         
-        self.sizeChangedF(w,h)
+        # self.sizeChangedF(w,h)
         self.update()
 
     # clicked events of weidgets
@@ -222,7 +283,6 @@ class PanelUI(QtWidgets.QMainWindow):
             return
 
         self.gridClickedF(x, y, self.getTarget())
-        #button.setStyleSheet(f"background-color: {self.__rgb2hex(255,0,0)}")
 
     def __changeTarget(self, item):
         rospy.loginfo(f'Select target {item.text()}.')
@@ -243,38 +303,21 @@ class PanelUI(QtWidgets.QMainWindow):
             return -1,-1
 
     # fucntions used from outside
-    def showPath(self, path, beg_color=(255,0,0), end_color=(0,0,255)):
-
-        self.__resetGridMap()
-        i,l = -1,len(path.path)
-        beg_color = np.array(beg_color)
-        end_color = np.array(end_color)
-
-        for p in path.path:
-            i+=1
-            fuse_color = beg_color*(1.0-i/l) + end_color*i/l
-            x, y = int(p.x), int(p.y)
-            self.__setButtonColor(rgb=fuse_color, x=x, y=y).setText(f'{p.id}')
-
+    def showPath(self, path):
+        self.draw_multi_targets = None
+        self.draw_target = None
+        self.draw_target = path
         self.update()
 
     # fucntions used from outside
     def showDashBoard(self, paths, allstatus):
-
-        self.__resetGridMap()
-
-        paths = paths.values()
-        rainbow = self.__rainbow(len(paths))
-        for color,path in zip(rainbow,paths):
-            status = allstatus[path.target]
-            for p in path.path[status.current.id: status.current.id+3]:
-                x, y = int(p.x), int(p.y)
-                self.__setButtonColor(rgb=color, x=x, y=y).setText(f'{p.id}')
-
+        self.draw_multi_targets = None
+        self.draw_target = None
+        self.draw_multi_targets = (paths, allstatus)
         self.update()
 
     def getTarget(self):
-        return self.listWidget.currentItem().text()
+        return self.targetlist.currentItem().text()
 
     def isDashBoardMode(self):
         return self.getTarget()=='dashboard'
@@ -294,7 +337,7 @@ class Console():
 
         self.status = {}
         self.path = {}
-        for item in [self.ui.listWidget.item(i) for i in range(self.ui.listWidget.count())]:
+        for item in [self.ui.targetlist.item(i) for i in range(self.ui.targetlist.count())]:
             if item.text() == 'dashboard':
                 continue
             self.path[item.text()] = Path()
